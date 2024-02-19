877fbba0e33130ee4e407e8590199895980facba
elbourkadi <abderrazzak.elbourkadi@edu.uae.ac.ma>
2024-02-19 03:30:51 +0100

    changes done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Abderrazzak</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop web applications , user <br className='sm:block hidden' />
-            interfaces and mobile applications
+            I develop web applications with Bubble
           </p>
         </div>
       </div>
